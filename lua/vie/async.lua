4c3398f8c046cf1a9f9ed54ea78d92caa30cbd52
AgatZan <125749924+AgatZan@users.noreply.github.com>
2025-12-18 22:20:56 +0300

    feat(async): add `non-blocking` snippet

--- lua/vie/async.lua
+++ lua/vie/async.lua
@@ -8,7 +8,8 @@ local function selector(choices)
 	---#if async
 	local co = coroutine.running()
 	local choicer = function(your_return)
-		vim.schedule( --- if inside function not `api-fast`
+		--- if inside function not `api-fast` or to prevent blocking
+		vim.schedule(
 			function()
 				coroutine.resume(co, your_return)
 			end
@@ -18,7 +19,6 @@ local function selector(choices)
 	-- local choicer = function(your_return)
 	-- 	coroutine.resume(co, your_return)
 	-- end
-	-- choicer = vim.schedule_wrap(choicer)
 	vim.ui.select(choices, {}, choicer)
 	return coroutine.yield()
 	---#else
@@ -29,3 +29,18 @@ end
 coroutine.wrap(function()
 	local choice = selector(choices)
 end)()
+::non-blocking::
+local co = coroutine.create(blocking_function)
+local function step()
+	vim.schedule(function()
+		local status, res_or_error = coroutine.resume(co)
+		--- error or coroutine is dead
+		if not status then
+			error(res_or_error)
+		elseif condition_to_continue(res_or_error) then
+			step()
+		end
+	end)
+end
+--- start execution
+step()
